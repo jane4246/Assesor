3a9f75b9ba5b88b50c39ed187ec7f9687a711037
maritimjane <58557449+jane4246@users.noreply.github.com>
2025-12-17 08:22:17 -0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,10 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
+import Signup from "@/pages/signup";
 import UploadPage from "@/pages/upload";
 import PaymentPage from "@/pages/payment";
 import EmailPage from "@/pages/email";
@@ -14,10 +16,17 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+
+      {/* Signup page */}
+      <Route path="/signup" component={Signup} />
+
+      {/* Existing flow */}
       <Route path="/upload" component={UploadPage} />
       <Route path="/payment/:id" component={PaymentPage} />
       <Route path="/email/:id" component={EmailPage} />
       <Route path="/admin" component={AdminPage} />
+
+      {/* 404 */}
       <Route component={NotFound} />
     </Switch>
   );
